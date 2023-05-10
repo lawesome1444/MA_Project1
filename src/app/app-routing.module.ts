4cef19831ed3fcb50b677f88ba5811ebc14d7b69
unknown <pacman1444@gmail.com>
2023-05-10 20:38:41 +0000

    Barely Bare Bones start for the Quick Reviews App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'game1',
+    loadChildren: () => import('./game1/game1.module').then( m => m.Game1PageModule)
   },
+
 ];
 
 @NgModule({
